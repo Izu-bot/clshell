0656030b0268966fb96f7d1099daa529d8df3145
Kauan Cardoso <kauanmartins977@gmail.com>
2026-09-19 14:52:12 -0300

    feat: inicia projeto e cria o método para ler entradas do usuário

---
+++ includes/clshell.h
@@ -0,0 +1,9 @@
+#ifndef CLSHELL_H
+#define CLSHELL_H
+
+#define CLS_BUFSIZE 1024
+
+void cls_loop();
+char* cls_read_line();
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../includes/clshell.h"
+
+int main()
+{
+    cls_loop();
+
+    return EXIT_SUCCESS;
+}
+
+void cls_loop()
+{
+    char *line;
+    int status;
+
+    do {
+        printf("> ");
+        line = cls_read_line();
+        printf("O usuário digitou isso > %s\n", line);
+
+    } while (status);
+}
+
+char* cls_read_line()
+{
+    int bufsize = CLS_BUFSIZE;
+    char* buffer = malloc(bufsize * sizeof(char));
+    int c;
+
+    if (buffer == NULL) {
+        fprintf(stderr, "Erro ao tentr alocar memória.");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < bufsize; i++) {
+        c = getchar();
+
+        if (c != EOF && c != '\n') {
+            buffer[i] = c;
+        } else {
+            buffer[i] = '\0';
+            break;
+        }
+    }
+
+    return buffer;
+}
